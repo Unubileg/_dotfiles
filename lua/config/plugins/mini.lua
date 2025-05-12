34e855b7f9841dbbece1ea496995b179c73acc46
Uno <the0hno@icloud.com>
2025-05-12 22:43:33 +0800

    update

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -39,12 +39,12 @@ return {
         },
       })
 
-      vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
+      -- vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" })
 
-      vim.keymap.set("n", "<leader>ef", function()
-        MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-        MiniFiles.reveal_cwd()
-      end, { desc = "Reveal current file in explorer" })
+      -- vim.keymap.set("n", "<leader>ef", function()
+      --   MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+      --   MiniFiles.reveal_cwd()
+      -- end, { desc = "Reveal current file in explorer" })
     end,
   },
 
